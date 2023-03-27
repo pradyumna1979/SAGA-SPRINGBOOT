f85624ec2d67ce8fd1c3c0749f921ea3fd515cb0
pradyumna1979 <kuni.khadanga@gmail.com>
2023-03-27 21:48:48 +0530

    Basic Saga spring boot framework

--- SERVICE-REGISTRY/src/test/java/com/service/registry/ServiceRegistryApplicationTests.java
+++ SERVICE-REGISTRY/src/test/java/com/service/registry/ServiceRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.saga;
+package com.service.registry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OrderServiceApplicationTests {
+class ServiceRegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
